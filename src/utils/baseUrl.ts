8197e77c9206be0880b75a7a60c65ebb336bc487
Sabid Hasan <sabid.soft@gmail.com>
2024-12-13 18:25:39 +0600

    base url added

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://avatar-management-system-server.vercel.app';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://japanese-vocabulary-server.vercel.app';
+// export const BASE_URL = 'http://localhost:8080';
